a7ef5726dc17e996c31d2e32a37cd3b5ee9d7452
Elena Akimova <kulikovaelena19@yandex.ru>
2024-04-18 22:07:05 +0300

    7commit

--- src/main/java/ru/kulikovaelena/Main.java
+++ src/main/java/ru/kulikovaelena/Main.java
@@ -3,6 +3,8 @@ package ru.kulikovaelena;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!");
-        System.out.println("How are you?");
+        {
+            System.out.println("How are you??");
+        }
     }
 }
